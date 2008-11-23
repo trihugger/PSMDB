395e27353263a3a06c5df131dd13ccb524d9c450
braindedd <braindedd@e0e46c49-be69-4f5a-ad62-21024a331aea>
2008-11-23 14:39:08 +0000

    Added DB from latest PSDB

--- Development/database/db_version.sql
+++ Development/database/db_version.sql
@@ -1,8 +1,8 @@
 -- MySQL dump 10.11
 --
--- Host: localhost    Database: silvermoon
+-- Host: localhost    Database: mangos
 -- ------------------------------------------------------
--- Server version	5.0.67
+-- Server version	5.0.51b-community-nt
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -24,7 +24,7 @@ SET @saved_cs_client     = @@character_set_client;
 SET character_set_client = utf8;
 CREATE TABLE `db_version` (
   `version` varchar(120) default NULL,
-  `required_2008_11_18_02_mangos_mangos_string` bit(1) default NULL
+  `required_2008_11_16_01_mangos_command` bit(1) default NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Used DB version notes';
 SET character_set_client = @saved_cs_client;
 
@@ -34,7 +34,7 @@ SET character_set_client = @saved_cs_client;
 
 LOCK TABLES `db_version` WRITE;
 /*!40000 ALTER TABLE `db_version` DISABLE KEYS */;
-INSERT INTO `db_version` VALUES ('PSDB x14 (448)',NULL);
+INSERT INTO `db_version` VALUES ('PSDB x14 (451)',NULL);
 /*!40000 ALTER TABLE `db_version` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -47,4 +47,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2008-11-19 17:24:55
+-- Dump completed on 2008-11-20 20:02:56
