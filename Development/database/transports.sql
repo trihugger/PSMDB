8081f31b77576d382fe0415d45b8f0f1e620724b
Rog360 <Rog360@e0e46c49-be69-4f5a-ad62-21024a331aea>
2008-12-03 18:41:41 +0000

    Added
    WOTLK Trainers - Marx
    
    WotLK Trainer Spells - Rog360 & Marx
    
    right transports - Marx

--- Development/database/transports.sql
+++ Development/database/transports.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.11
 --
--- Host: localhost    Database: mangos
+-- Host: localhost    Database: mangos2
 -- ------------------------------------------------------
 -- Server version	5.0.51b-community-nt
 
@@ -36,7 +36,7 @@ SET character_set_client = @saved_cs_client;
 
 LOCK TABLES `transports` WRITE;
 /*!40000 ALTER TABLE `transports` DISABLE KEYS */;
-INSERT INTO `transports` VALUES (176495,'Grom\'Gol Base Camp and Undercity',315032),(176310,'Menethil Harbor and Auberdine',241778),(176244,'Teldrassil and Auberdine',309295),(176231,'Menethil Harbor and Theramore Isle',230162),(175080,'Grom\'Gol Base Camp and Orgrimmar',248990),(164871,'Orgrimmar and Undercity',239334),(20808,'Ratchet and Booty Bay',231236),(177233,'Forgotton Coast and Feathermoon Stronghold',259751),(181646,'Azuremyst and Auberdine',238707);
+INSERT INTO `transports` VALUES (176495,'Grom\'Gol Base Camp and Undercity',315032),(176310,'Menethil Harbor and Auberdine',241778),(176244,'Teldrassil and Auberdine',309295),(176231,'Menethil Harbor and Theramore Isle',230162),(175080,'Grom\'Gol Base Camp and Orgrimmar',248990),(164871,'Orgrimmar and Undercity',239334),(20808,'Ratchet and Booty Bay',231236),(177233,'Forgotton Coast and Feathermoon Stronghold',259751),(181646,'Azuremyst and Auberdine',238707),(190536,'Valiance Keep and Stormwind',271979),(181688,'Valgarde and Menethil',445534),(181689,'Undercity and Vengeance Landing',214579),(186238,'Orgrimmar and Warsong Hold',302705),(186371,'Stolen Zeppelin',484211),(187568,'Moa\'ki Harbor Turtle Boat',445220),(187038,'Pirate boat',307953),(188511,'Unu\'pe Turtle Boat',502354),(192241,'Orgrims Hammer',1424158),(192242,'Fizzcrank Airstrip',1051387);
 /*!40000 ALTER TABLE `transports` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -49,4 +49,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2008-06-06 21:19:16
+-- Dump completed on 2008-12-03 18:36:38
