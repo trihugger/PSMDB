9751f97f1c41a5916ae56c9da1cd498f18c2bad7
Rog360 <Rog360@e0e46c49-be69-4f5a-ad62-21024a331aea>
2008-12-10 02:34:08 +0000

    More items... :)

--- Development/database/db_version.sql
+++ Development/database/db_version.sql
@@ -34,7 +34,7 @@ SET character_set_client = @saved_cs_client;
 
 LOCK TABLES `db_version` WRITE;
 /*!40000 ALTER TABLE `db_version` DISABLE KEYS */;
-INSERT INTO `db_version` VALUES ('PSDB WotLK (30)',NULL);
+INSERT INTO `db_version` VALUES ('PSDB WotLK (31)',NULL);
 /*!40000 ALTER TABLE `db_version` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -47,4 +47,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2008-12-09 21:11:11
+-- Dump completed on 2008-12-10  2:32:57
