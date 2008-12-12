ed7c109a2c116f3eafa51ba9805770ce7f58e4ce
Rog360 <Rog360@e0e46c49-be69-4f5a-ad62-21024a331aea>
2008-12-12 19:04:44 +0000

    Added more quests,Item fixes, and page texts.

--- Development/database/db_version.sql
+++ Development/database/db_version.sql
@@ -34,7 +34,7 @@ SET character_set_client = @saved_cs_client;
 
 LOCK TABLES `db_version` WRITE;
 /*!40000 ALTER TABLE `db_version` DISABLE KEYS */;
-INSERT INTO `db_version` VALUES ('PSDB WotLK (36)',NULL);
+INSERT INTO `db_version` VALUES ('PSDB WotLK (38)',NULL);
 /*!40000 ALTER TABLE `db_version` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -47,4 +47,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2008-12-12  6:53:19
+-- Dump completed on 2008-12-12 19:01:20
