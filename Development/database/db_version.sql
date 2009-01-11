754f21beb59255168b4150cdde8c1ff12d4f27c4
Rog360 <Rog360@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-01-11 06:48:08 +0000

    Added a few GOs and spawn fixes...

--- Development/database/db_version.sql
+++ Development/database/db_version.sql
@@ -24,7 +24,7 @@ SET @saved_cs_client     = @@character_set_client;
 SET character_set_client = utf8;
 CREATE TABLE `db_version` (
   `version` varchar(120) default NULL,
-  `required_7061_01_mangos_spell_proc_event` bit(1) default NULL
+  `required_7067_02_mangos_spell_learn_spell` bit(1) default NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Used DB version notes';
 SET character_set_client = @saved_cs_client;
 
@@ -34,7 +34,7 @@ SET character_set_client = @saved_cs_client;
 
 LOCK TABLES `db_version` WRITE;
 /*!40000 ALTER TABLE `db_version` DISABLE KEYS */;
-INSERT INTO `db_version` VALUES ('PSDB WotLK (61)',NULL);
+INSERT INTO `db_version` VALUES ('PSDB WotLK (62)',NULL);
 /*!40000 ALTER TABLE `db_version` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -47,4 +47,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2009-01-10 18:18:25
+-- Dump completed on 2009-01-11  6:46:21
