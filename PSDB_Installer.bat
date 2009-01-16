abcb75fe0cd0d7b41da0b94dabfdfbfa238abce3
braindedd <braindedd@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-01-16 20:57:24 +0000

    Major updates to batch file and new optimizer tool

--- PSDB_Installer.bat
+++ PSDB_Installer.bat
@@ -1,8 +1,26 @@
 @echo off
-
+:quick
+rem Quick install section
+rem This will automatically use the variables below to install the world and scriptdev2 databases without prompting then optimize them and exit
+rem To use: Set your environment variables below and change 'set quick=off' to 'set quick=on' 
+set quick=off
+if %quick% == off goto standard
+echo (( PSDB Quick Installer ))
+rem -- Change the values below to match your server --
+set svr=localhost
+set user=root
+set pass=
+set port=3306
+set wdb=mangos
+set sd2db=scriptdev2
+rem -- Don't change past this point --
+set yesno=y
+goto install
+:standard
+rem Standard install section
 color 3
-echo.
-echo 8888888b.   .d8888b.  8888888b.  888888b.
+echo .
+echo 8888888b.   .d8888b.  8888888b.  888888b.  (LK)
 echo 888   Y88b d88P  Y88b 888  "Y88b 888  "88b
 echo 888    888 Y88b.      888    888 888  .88P
 echo 888   d88P  "Y888b.   888    888 8888888K.
@@ -10,72 +28,99 @@ echo 8888888P"      "Y88b. 888    888 888  "Y88b
 echo 888              "888 888    888 888    888
 echo 888        Y88b  d88P 888  .d88P 888   d88P
 echo 888         "Y8888P"  8888888P"  8888888P"
+echo.
 echo Credits to: Factionwars, Nemok and BrainDedd
-pause
 echo.
-echo What is your MySQL Host Name? (By default it's localhost)
-set /p svr=
-echo.
-echo What is your MySQL Username? (By default it's root)
-set /p user=
-echo.
-echo What is your MySQL Password? (By default it's nothing)
-set /p pass=
-echo.
-echo What is your MySQL Port? (By default it's 3306)
-set /p port=
-echo.
-echo What is your World Database Name? (By default Mangos)
-set /p wdb=
-echo.
-echo What is your ScriptDev2 Database Name? (By default scriptdev2)
-set /p sd2db=
-echo.
-echo What is your Characters Database Name? (By default characters)
-set /p cdb=
-echo.
-echo What is your Realmd Database Name? (By default realmd)
-set /p rdb=
+set /p svr=What is your MySQL host name?           [localhost]   : 
+if %svr%. == . set svr=localhost
+set /p user=What is your MySQL user name?           [root]        : 
+if %user%. == . set user=root
+set /p pass=What is your MySQL password?            [ ]           : 
+if %pass%. == . set pass=
+set /p port=What is your MySQL port?                [3306]        : 
+if %port%. == . set port=3306
+set /p wdb=What is your World database name?       [mangos]      : 
+if %wdb%. == . set wdb=mangos
+set /p sd2db=What is your ScriptDev2 database name?  [scriptdev2]  : 
+if %sd2db%. == . set sd2db=scriptdev2
+set /p cdb=What is your Characters database name?  [characters]  : 
+if %cdb%. == . set cdb=characters
+set /p rdb=What is your Realmd database name?      [realmd]      : 
+if %rdb%. == . set rdb=realmd
 
-rem -- Don't change anything below this line --
-
+:install
 set dbpath=Development\database
+set optim=Tools\DB_Optimizer
 set mysql=.
 
-echo.
-echo This will wipe out your current World database and replace it.
-set /p yesno=Do you wish to continue? (y/n) 
-if %yesno% neq y if %yesno% neq Y goto phase2
+:world
+if %quick% == off echo.
+if %quick% == off echo This will wipe out your current World database and replace it.
+if %quick% == off set /p yesno=Do you wish to continue? (y/n) 
+if %quick% == off if %yesno% neq y if %yesno% neq Y goto sd2
 
 echo.
-echo Importing DB, please wait ...
+echo Importing World database
 
-for %%i in (%dbpath%\*.sql) do if %%i neq %dbpath%\scriptdev2.sql if %%i neq %dbpath%\characters.sql if %%i neq %dbpath%\realmd.sql echo %%i & %mysql%\mysql -h %svr% --user=%user% --password=%pass% --port=%port% %wdb% < %%i
+for %%i in (%dbpath%\*.sql) do if %%i neq %dbpath%\scriptdev2.sql if %%i neq %dbpath%\characters.sql if %%i neq %dbpath%\realmd.sql echo %%i & %mysql%\mysql -q -s -h %svr% --user=%user% --password=%pass% --port=%port% %wdb% < %%i
 
-:phase2
+:sd2
+if %quick% == off echo.
+if %quick% == off echo This will wipe out your current ScriptDev2 database and replace it.
+if %quick% == off set /p yesno=Do you wish to continue? (y/n) 
+if %quick% == off if %yesno% neq y if %yesno% neq Y goto characters
+
 echo.
-echo This will wipe out your current ScriptDev2 database and replace it.
-set /p yesno=Do you wish to continue? (y/n) 
-if %yesno% neq y if %yesno% neq Y goto phase3
-echo Importing...
-%mysql%\mysql -h %svr% --user=%user% --password=%pass% --port=%port% %sd2db% < %dbpath%\scriptdev2.sql
+echo Importing ScriptDev2 database
 
-:phase3
-echo.
-echo This will wipe out your current Realm database and replace it.
-set /p yesno=Do you wish to continue? (y/n) 
-if %yesno% neq y if %yesno% neq Y goto phase4
-echo Importing...
-%mysql%\mysql -h %svr% --user=%user% --password=%pass% --port=%port% %rdb% < %dbpath%\realmd.sql
-:phase4
+%mysql%\mysql -q -s -h %svr% --user=%user% --password=%pass% --port=%port% %sd2db% < %dbpath%\scriptdev2.sql
+
+if %quick% neq off goto optimize
+
+:characters
 echo.
 echo This will wipe out your current Characters database and replace it.
 set /p yesno=Do you wish to continue? (y/n) 
-if %yesno% neq y if %yesno% neq Y goto exit
-echo Importing...
-%mysql%\mysql -h %svr% --user=%user% --password=%pass% --port=%port% %cdb% < %dbpath%\characters.sql
+if %yesno% neq y if %yesno% neq Y goto realm
 
 echo.
-echo Done :)
+echo Importing Characters database
 
-:exit
+%mysql%\mysql -q -s -h %svr% --user=%user% --password=%pass% --port=%port% %cdb% < %dbpath%\characters.sql
+
+:realm
+echo.
+echo This will wipe out your current Realm database and replace it.
+set /p yesno=Do you wish to continue? (y/n) 
+if %yesno% neq y if %yesno% neq Y goto optimize
+
+echo.
+echo Importing Realm database
+
+%mysql%\mysql -q -s -h %svr% --user=%user% --password=%pass% --port=%port% %rdb% < %dbpath%\realmd.sql
+
+:optimize
+if %quick% == off echo.
+if %quick% == off echo This will optimize your current database.
+if %quick% == off set /p yesno=Do you wish to continue? (y/n) 
+if %quick% == off if %yesno% neq y if %yesno% neq Y goto done
+
+echo.
+echo Optimizing database
+
+%optim%\Optimizer.exe
+copy %optim%\scriptdev2_optimize.sql . >nul
+echo World
+%mysql%\mysql -q -s -h %svr% --user=%user% --password=%pass% --port=%port% %wdb% < optimize.sql >nul
+echo ScriptDev2
+%mysql%\mysql -q -s -h %svr% --user=%user% --password=%pass% --port=%port% %sd2db% < scriptdev2_optimize.sql >nul
+del optimize.sql
+del scriptdev2_optimize.sql
+
+if %quick% neq off goto eof
+
+:done
+echo.
+echo Done :)
+echo.
+pause
---
+++ Tools/DB_Optimizer/scriptdev2_optimize.sql
@@ -0,0 +1,6 @@
+OPTIMIZE TABLE custom_texts;
+OPTIMIZE TABLE eventai_scripts;
+OPTIMIZE TABLE eventai_summons;
+OPTIMIZE TABLE eventai_texts;
+OPTIMIZE TABLE script_texts;
+OPTIMIZE TABLE sd2_db_version;
