cd8ec8c241d14f92e11ec2965e9f2c65313dd70d
Rog360 <Rog360@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-01-18 10:22:20 +0000

    Few more fixes...

--- Development/database/db_version.sql
+++ Development/database/db_version.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: mangos
 -- ------------------------------------------------------
--- Server version	5.0.51b-community-nt-log
+-- Server version	5.0.45-community-nt-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -34,7 +34,7 @@ SET character_set_client = @saved_cs_client;
 
 LOCK TABLES `db_version` WRITE;
 /*!40000 ALTER TABLE `db_version` DISABLE KEYS */;
-INSERT INTO `db_version` VALUES ('PSDB WotLK (77)',NULL);
+INSERT INTO `db_version` VALUES ('PSDB WotLK (78)',NULL);
 /*!40000 ALTER TABLE `db_version` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -47,4 +47,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2009-01-18  9:21:03
+-- Dump completed on 2009-01-18 10:19:51
