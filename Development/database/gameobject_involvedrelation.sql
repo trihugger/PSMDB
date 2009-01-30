bf3f93223dfe232212c4c801b1a85742fa5c2617
Rog360 <Rog360@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-01-30 01:44:22 +0000

    Added alot of fixes by Mephilith... :)

--- Development/database/gameobject_involvedrelation.sql
+++ Development/database/gameobject_involvedrelation.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: mangos
 -- ------------------------------------------------------
--- Server version	5.0.51b-community-nt-log
+-- Server version	5.0.45-community-nt-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -48,4 +48,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2009-01-15 18:59:30
+-- Dump completed on 2009-01-30  1:30:50
