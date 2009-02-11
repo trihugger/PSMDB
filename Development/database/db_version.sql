35cce0afa031aee99803d948f39e4a9c0e6b388b
Rog360 <Rog360@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-02-11 01:28:46 +0000

    Added npc_trainer fixes for hunter trainers and a few from forum... :)

--- Development/database/db_version.sql
+++ Development/database/db_version.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.1.31, for Win32 (ia32)
+-- MySQL dump 10.11
 --
--- Host: localhost    Database: slk
+-- Host: localhost    Database: mangos
 -- ------------------------------------------------------
--- Server version	5.1.31
+-- Server version	5.0.45-community-nt-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -23,8 +23,8 @@ DROP TABLE IF EXISTS `db_version`;
 SET @saved_cs_client     = @@character_set_client;
 SET character_set_client = utf8;
 CREATE TABLE `db_version` (
-  `version` varchar(120) DEFAULT NULL,
-  `required_7252_02_mangos_mangos_string` bit(1) DEFAULT NULL
+  `version` varchar(120) default NULL,
+  `required_7252_02_mangos_mangos_string` bit(1) default NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Used DB version notes';
 SET character_set_client = @saved_cs_client;
 
@@ -34,7 +34,7 @@ SET character_set_client = @saved_cs_client;
 
 LOCK TABLES `db_version` WRITE;
 /*!40000 ALTER TABLE `db_version` DISABLE KEYS */;
-INSERT INTO `db_version` VALUES ('PSDB WotLK (119)',NULL);
+INSERT INTO `db_version` VALUES ('PSDB WotLK (120)',NULL);
 /*!40000 ALTER TABLE `db_version` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -47,4 +47,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2009-02-08 20:12:18
+-- Dump completed on 2009-02-11  1:25:52
