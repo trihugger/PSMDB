34abc85a152bacde35e4ae18a883b34d13362a97
braindedd <braindedd@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-04-23 17:03:52 +0000

    Minor ScriptDev2 update - unneeded tables removed

--- Development/database/db_version.sql
+++ Development/database/db_version.sql
@@ -35,7 +35,7 @@ SET character_set_client = @saved_cs_client;
 
 LOCK TABLES `db_version` WRITE;
 /*!40000 ALTER TABLE `db_version` DISABLE KEYS */;
-INSERT INTO `db_version` VALUES ('PSDB WotLK (184)','PSDB EventAI',NULL);
+INSERT INTO `db_version` VALUES ('PSDB WotLK (185)','PSDB EventAI',NULL);
 /*!40000 ALTER TABLE `db_version` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
