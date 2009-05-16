48ada784addc86f494f6f762b14de394c4a4df34
braindedd <braindedd@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-05-16 17:09:55 +0000

    Updated to MaNGOS r7830 / ScriptDev2 r1082 Minor fixes to ScriptDev2 optimizer

--- Tools/DB_Optimizer/scriptdev2_optimize.sql
+++ Tools/DB_Optimizer/scriptdev2_optimize.sql
@@ -1,6 +1,5 @@
 OPTIMIZE TABLE custom_texts;
-OPTIMIZE TABLE eventai_scripts;
-OPTIMIZE TABLE eventai_summons;
-OPTIMIZE TABLE eventai_texts;
+OPTIMIZE TABLE script_localized_texts;
 OPTIMIZE TABLE script_texts;
+OPTIMIZE TABLE script_waypoint;
 OPTIMIZE TABLE sd2_db_version;
