eebea46a3f6692d844f2c3cc4ccfb638ec64a7a2
Rog360 <Rog360@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-06-04 21:00:20 +0000

    Added to svn 211... :)
    -- Added GM Island Vendors Back with phasemask 2. So have to have GM on to see them.
    -- Npc 24008
    -- fix Quest 12484
    -- Fix Quest 12449
    -- Alfred Copperworth <Butler>
    -- Conjurer Weinhaus
    -- Archmage John Nicholas -
    -- Minor Fix NPC health & level
    -- Warmaul Shaman / Wrnog Health
    -- Warmaul Brute / Wrong Health
    -- Warmaul Warlock / Wrong Health
    -- Fel Cannon: Hate / Wrong health
    -- Quartermaster Bartlett <Blacksmithing Supplies>
    -- Patchwerk
    -- Grobbulus
    -- Fix Npc Facion in Hyjal
    -- Item 36742&36746 Must be part of the specified team
    -- Lord Irulon Trueblade <The Argent Crusade> / Must Be Gossip
    -- Small Fix For Quest
    -- Elune's Blessing
    -- Fix Quest The Story Thus Far...
    -- Minor Fix and more...
    -- Dark Assault - Alliance Portal - Invisible Stalker / Wrong Faction
    -- Dark Assault - Horde Portal - Invisible Stalker /Wrong Faction
    -- Explodyne Fizzlespurt <Arena Vendor> / Only Sell 60 Item
    -- Strengthen the Ancients / Must Be prevquest id
    -- Pattern: Windripper Leggings & Pattern: Windripper Boots / Must Be Flag Change -
    -- This Item's No PvP_Item
    -- This Item Must be change Flag
    -- This Mobs Shouldn't go there
    -- patrol 1 from construct quarter naxx
    -- patrol 2 from construct quarter naxx
    -- another patrol from construc quarter
    -- This Group Doesn't be there
    -- Another Group
    -- Cleaning Patchwerk Zone So Spawned
    -- cleaning up patchwerk way embalming slime
    -- cleaning up patchwerk way sewage slime

--- Development/database/db_version.sql
+++ Development/database/db_version.sql
@@ -35,7 +35,7 @@ SET character_set_client = @saved_cs_client;
 
 LOCK TABLES `db_version` WRITE;
 /*!40000 ALTER TABLE `db_version` DISABLE KEYS */;
-INSERT INTO `db_version` VALUES ('PSDB WotLK (210)','PSDB EventAI',NULL);
+INSERT INTO `db_version` VALUES ('PSDB WotLK (211)','PSDB EventAI',NULL);
 /*!40000 ALTER TABLE `db_version` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -48,4 +48,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2009-05-30 21:53:24
+-- Dump completed on 2009-06-04 20:51:10
