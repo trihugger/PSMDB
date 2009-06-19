cd8f3acaeda33d821e930b003c47530b5a84d198
braindedd <braindedd@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-06-19 17:36:53 +0000

    Updated to MaNGOS r8030 / ScriptDev2 r1149

--- Development/database/npc_spellclick_spells.sql
+++ Development/database/npc_spellclick_spells.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.11
+-- MySQL dump 10.13  Distrib 5.1.35, for Win32 (ia32)
 --
--- Host: localhost    Database: mangos
+-- Host: localhost    Database: slk
 -- ------------------------------------------------------
--- Server version	5.1.32-community
+-- Server version	5.1.35
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -20,15 +20,17 @@
 --
 
 DROP TABLE IF EXISTS `npc_spellclick_spells`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `npc_spellclick_spells` (
   `npc_entry` int(10) unsigned NOT NULL COMMENT 'reference to creature_template',
   `spell_id` int(10) unsigned NOT NULL COMMENT 'spell which should be casted ',
-  `quest_id` int(10) unsigned NOT NULL COMMENT 'reference to quest_template',
+  `quest_start` mediumint(8) unsigned NOT NULL COMMENT 'reference to quest_template',
+  `quest_start_active` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `quest_end` mediumint(8) unsigned NOT NULL DEFAULT '0',
   `cast_flags` tinyint(3) unsigned NOT NULL COMMENT 'first bit defines caster: 1=player, 0=creature; second bit defines target, same mapping as caster bit'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-SET character_set_client = @saved_cs_client;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `npc_spellclick_spells`
@@ -36,7 +38,7 @@ SET character_set_client = @saved_cs_client;
 
 LOCK TABLES `npc_spellclick_spells` WRITE;
 /*!40000 ALTER TABLE `npc_spellclick_spells` DISABLE KEYS */;
-INSERT INTO `npc_spellclick_spells` VALUES (28389,51592,12605,1),(28389,51593,12605,3),(26477,47096,11999,3),(26477,47096,12000,3),(26477,51592,11999,1),(26477,51592,12000,1),(29856,55363,12643,3),(29856,55363,12629,3),(29856,51592,12643,1),(29856,51592,12629,1),(26200,46773,11960,3),(26200,51592,11960,1),(28162,50737,12519,3),(28162,51592,12519,1),(28202,50927,12527,3),(28202,51592,12527,1),(28203,50919,12527,3),(28203,51592,12527,1),(22258,38708,10838,3),(22258,51592,10838,1),(29154,53335,0,3),(29501,54575,0,3),(29488,54568,0,3);
+INSERT INTO `npc_spellclick_spells` VALUES (28389,51592,12605,1,12605,1),(28389,51593,12605,1,12605,3),(26477,47096,11999,1,11999,3),(26477,47096,12000,1,12000,3),(26477,51592,11999,1,11999,1),(26477,51592,12000,1,12000,1),(29856,55363,12643,1,12643,3),(29856,55363,12629,1,12629,3),(29856,51592,12643,1,12643,1),(29856,51592,12629,1,12629,1),(26200,46773,11960,1,11960,3),(26200,51592,11960,1,11960,1),(28162,50737,12519,1,12519,3),(28162,51592,12519,1,12519,1),(28202,50927,12527,1,12527,3),(28202,51592,12527,1,12527,1),(28203,50919,12527,1,12527,3),(28203,51592,12527,1,12527,1),(22258,38708,10838,1,10838,3),(22258,51592,10838,1,10838,1),(29154,53335,0,0,0,3),(29501,54575,0,0,0,3),(29488,54568,0,0,0,3);
 /*!40000 ALTER TABLE `npc_spellclick_spells` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -49,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2009-06-12 23:51:18
+-- Dump completed on 2009-06-19 17:33:11
