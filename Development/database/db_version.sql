890b550313ced251a1be2835f05406bd712af2d8
Rog360 <Rog360@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-07-02 20:19:56 +0000

    Added to svn 223... :)
    -- Gobject spawn fixes
    -- Quest Mobs set spawntime at 300
    -- Boss Tho'grun Loot 
    -- Cooking Trainers and Vendor Updates
    -- Divine Spirit added to trainers level 30 or higher.
    -- Dirge Quikcleave Quest Clamlette Surprise now only teaches Clamlette Magnifique. NPC also teaches it
    -- Grub now sells [Recipe: Dig Rat Stew]
    -- Prospector Khazgorm now sells the recipe for Dig Rat Stew to the Alliance.
    -- Mercurial Stone changes
    -- Artisan Cooking now taught by Trainers
    -- Remove Cookbooks from vendors
    -- Sunreaver Guardian stats.
    -- Innkeeper stats.
    -- NPCs in Horde Tent stats.
    -- Stabled Animals stats.
    -- Bird Now not attackble to avoid taming also passive
    -- NPC Champions H stats.
    -- NPC Champions A stats.
    -- FA Trainer spells
    -- Argent Peacekeeper stats.
    -- Other random Argent NPCs stats.
    -- Another Needing Mount
    -- Flight Master stats.
    -- Silver Covenant Guardian stats.
    -- Alliance NPCs stats.
    -- Innkeeper stats.
    -- Stable Master stats.
    -- Argent Trainers & Tradeskill Vendors and Argent Tent NPCs stats.
    -- (item bugged ) exploit (Mephilith)
    -- [BUG] Declaration of Power (9666)
    -- Iron Rune Avenger stats
    -- Revantusk Watcher stats
    -- Illidari Nightlord stats
    -- Deathcharger Steed some stats
    -- Fallout Slime stats
    -- Jungle Panther stats
    -- Frostworg stats
    -- Oloh stats and spawn
    -- King Krush stats,spawn and wp (Used missing guid)
    -- Aotona stats,spawn and wp (Used missing guid)
    -- Terror Spinner stats and spawn (Used missing guid)
    -- Tukemuth ,spawn and wp (Used missing guid)
    -- Gameobject type 33 Temp fix crash

--- Development/database/db_version.sql
+++ Development/database/db_version.sql
@@ -35,7 +35,7 @@ SET character_set_client = @saved_cs_client;
 
 LOCK TABLES `db_version` WRITE;
 /*!40000 ALTER TABLE `db_version` DISABLE KEYS */;
-INSERT INTO `db_version` VALUES ('PSDB WotLK (222)','PSDB EventAI',NULL);
+INSERT INTO `db_version` VALUES ('PSDB WotLK (223)','PSDB EventAI',NULL);
 /*!40000 ALTER TABLE `db_version` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -48,4 +48,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2009-06-26 19:00:51
+-- Dump completed on 2009-07-02 20:11:20
