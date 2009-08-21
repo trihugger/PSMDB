eb6c322e82ac9ffeec31b2f0a3234fdf6a6bd026
braindedd <braindedd@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-08-21 22:12:24 +0000

    Updated to MaNGOS r8402 / ScriptDev2 r1358

--- Development/database/spell_elixir.sql
+++ Development/database/spell_elixir.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.11
+-- MySQL dump 10.13  Distrib 5.1.37, for Win32 (ia32)
 --
 -- Host: localhost    Database: slk
 -- ------------------------------------------------------
--- Server version	5.0.67
+-- Server version	5.1.37
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -20,14 +20,14 @@
 --
 
 DROP TABLE IF EXISTS `spell_elixir`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `spell_elixir` (
-  `entry` int(11) unsigned NOT NULL default '0' COMMENT 'SpellId of potion',
-  `mask` tinyint(1) unsigned NOT NULL default '0' COMMENT 'Mask 0x1 battle 0x2 guardian 0x3 flask 0x7 unstable flasks 0xB shattrath flasks',
-  PRIMARY KEY  (`entry`)
+  `entry` int(11) unsigned NOT NULL DEFAULT '0' COMMENT 'SpellId of potion',
+  `mask` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT 'Mask 0x1 battle 0x2 guardian 0x3 flask 0x7 unstable flasks 0xB shattrath flasks',
+  PRIMARY KEY (`entry`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Spell System';
-SET character_set_client = @saved_cs_client;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `spell_elixir`
@@ -35,7 +35,7 @@ SET character_set_client = @saved_cs_client;
 
 LOCK TABLES `spell_elixir` WRITE;
 /*!40000 ALTER TABLE `spell_elixir` DISABLE KEYS */;
-INSERT INTO `spell_elixir` VALUES (41608,11),(41609,11),(41610,11),(41611,11),(40567,7),(40568,7),(40572,7),(40573,7),(40575,7),(40576,7),(17624,3),(17626,3),(17627,3),(17629,3),(17628,3),(28518,3),(28519,3),(28520,3),(28521,3),(28540,3),(42735,3),(2367,1),(2374,1),(3160,1),(3164,1),(7844,1),(8212,1),(10667,1),(10669,1),(11328,1),(11334,1),(11390,1),(11405,1),(11406,1),(11474,1),(16322,1),(16323,1),(16329,1),(17038,1),(17537,1),(17538,1),(17539,1),(21920,1),(26276,1),(28486,1),(28488,1),(28490,1),(28491,1),(28493,1),(28497,1),(28501,1),(28503,1),(33720,1),(33721,1),(33726,1),(38954,1),(673,2),(2378,2),(2380,2),(3166,2),(3219,2),(3220,2),(3222,2),(3223,2),(3593,2),(10668,2),(10692,2),(10693,2),(11319,2),(11348,2),(11349,2),(16321,2),(11364,2),(11371,2),(11396,2),(15231,2),(15233,2),(16325,2),(16326,2),(16327,2),(17535,2),(24361,2),(24363,2),(24382,2),(24383,2),(24417,2),(27652,2),(27653,2),(28502,2),(28509,2),(28514,2),(29348,2),(39625,2),(39626,2),(39627,2),(39628,2),(46837,11),(46839,11),(45373,1);
+INSERT INTO `spell_elixir` VALUES (41608,11),(41609,11),(41610,11),(41611,11),(40567,7),(40568,7),(40572,7),(40573,7),(40575,7),(40576,7),(17624,3),(17626,3),(17627,3),(17629,3),(17628,3),(28518,3),(28519,3),(28520,3),(28521,3),(28540,3),(42735,3),(2367,1),(2374,1),(3160,1),(3164,1),(7844,1),(8212,1),(10667,1),(10669,1),(11328,1),(11334,1),(11390,1),(11405,1),(11406,1),(11474,1),(16322,1),(16323,1),(16329,1),(17038,1),(17537,1),(17538,1),(17539,1),(21920,1),(26276,1),(28486,1),(28488,1),(28490,1),(28491,1),(28493,1),(28497,1),(28501,1),(28503,1),(33720,1),(33721,1),(33726,1),(38954,1),(673,2),(2378,2),(2380,2),(3166,2),(3219,2),(3220,2),(3222,2),(3223,2),(3593,2),(10668,2),(10692,2),(10693,2),(11319,2),(11348,2),(11349,2),(16321,2),(11364,2),(11371,2),(11396,2),(15231,2),(15233,2),(16325,2),(16326,2),(16327,2),(17535,2),(24361,2),(24363,2),(24382,2),(24383,2),(24417,2),(27652,2),(27653,2),(28502,2),(28509,2),(28514,2),(29348,2),(39625,2),(39626,2),(39627,2),(39628,2),(46837,11),(46839,11),(45373,1),(18191,16),(18192,16),(18193,16),(18194,16),(18222,16),(22730,16),(25661,16);
 /*!40000 ALTER TABLE `spell_elixir` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -48,4 +48,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2008-12-02 18:45:21
+-- Dump completed on 2009-08-22  0:08:53
