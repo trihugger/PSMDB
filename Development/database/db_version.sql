5623e140df97d0054330a7fdef76117aa34dff94
Rog360 <Rog360@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-08-31 15:56:49 +0000

    Added to svn 245... :)
    -- SFK Spawns 100% redone.
    -- How To Win Friends And Influence Enemies [1 Keleseth's Persuader]
    -- Blood Guard Zar'shi Armor Quartermaster
    -- Nargle Lashcord Veteran Arena Vendor
    -- Argex Irongut Veteran Arena Vendor Needs Spawnpoint
    -- Kezzik the Striker Veteran Arena Vendor Needs Spawnpoint
    -- Ecton Brasstumbler Apprentice Arena Vendor
    -- Knight-Lieutenant Moonstrike Armor Quartermaster
    -- Zom Bocom Apprentice Arena Vendor
    -- Doris Volanthius Veteran Armor Quartermaster
    -- Lieutenant Tristia Veteran Armor Quartermaster
    -- Master Sergeant Biggins
    -- Brave Stonehide
    -- Vixton Pinchwhistle
    -- Leeni "Smiley" Smalls
    -- Sergeant Thunderhorn
    -- Captain Dirgehammer
    -- Arcanist Firael Spawned Unused Guid
    -- Text Fix
    -- Jaxxil Sparks Holiday Fireworks Vendor
    -- William Saldean not spawned
    -- area52 special
    -- Recipe: Mighty Arcane Protection Potion
    -- q10792
    -- q10813
    -- [Item][10593,8244] Loot chance.
    -- free quest at wintergrasp missing specialflags
    -- q11719
    -- missing spellfocusGO
    -- [NPC][3927,3886] Dupe
    -- fix enchanted vial
    -- fix Dalaran Portal to Caverns of Time
    -- missing spel_target position (59901) for Dalaran Portal to Caverns of Time
    -- Abandoned Adventurer's Satchel
    -- Quest Flight master only - taxi sent by spell
    -- q12440
    -- s49511 Gryphon Taxi to Stars' Rest
    -- TaxipPath.dbc 884,285,286,0,
    -- Quest Flight master only - taxi sent by spell
    -- q12298
    -- s48862 High Commander Halford Wyrmbane: Westguard Keep to Wintergarde Keep Taxi
    -- TaxipPath.dbc 876,269,270,0
    -- Quest Flight master only - taxi sent by spell
    -- q12174
    -- s48013 Hippogryph Taxi to Wintergarde 
    -- TaxipPath.dbc 868,203,261,0,
    -- c27425
    -- Located upon a little tower next to the "Water Flume". Allows you to travel from Blue Sky Logging Ground to Venture Bay via a Log flume type flight path.
    -- temp TaxiPath quest 
    -- s48961 Log Ride Horde 00 
    -- TaxiPath.dbc 877,271,272,0,
    -- Alliance side
    -- c27414
    -- Located upon a little tower next to the "Water Flume". Allows you to travel from Blue Sky Logging Ground to Venture Bay via a Log flume type flight path.
    -- temp TaxiPath quest 
    -- s48621 Log Ride Alliance 01
    -- TaxiPath.dbc 869,262,263,0,
    -- temp TaxiPath quest 
    -- q11916
    -- s46621 Worg Taxi
    -- TaxiPath.dbc 805,232,233,0,
    -- TaxiPath.dbc 811,242,243,0,
    -- q12028
    -- spell allready sent by item at use
    -- required completion script by external event missing specialflags = 2 btw
    -- required link s47189 morph in whisp form buff Spiritual Insight
    -- book of glyph mastery

--- Development/database/db_version.sql
+++ Development/database/db_version.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.1.37, for Win32 (ia32)
+-- MySQL dump 10.11
 --
--- Host: localhost    Database: slk
+-- Host: localhost    Database: mangos
 -- ------------------------------------------------------
--- Server version	5.1.37
+-- Server version	5.1.32-community
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -20,15 +20,15 @@
 --
 
 DROP TABLE IF EXISTS `db_version`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `db_version` (
   `version` varchar(120) DEFAULT NULL,
   `creature_ai_version` varchar(120) DEFAULT NULL,
   `cache_id` int(10) DEFAULT '0',
   `required_8416_01_mangos_spell_learn_spell` bit(1) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Used DB version notes';
-/*!40101 SET character_set_client = @saved_cs_client */;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Dumping data for table `db_version`
@@ -36,7 +36,7 @@ CREATE TABLE `db_version` (
 
 LOCK TABLES `db_version` WRITE;
 /*!40000 ALTER TABLE `db_version` DISABLE KEYS */;
-INSERT INTO `db_version` VALUES ('PSDB WotLK (244)','PSDB EventAI & ACID 3.0.0',0,NULL);
+INSERT INTO `db_version` VALUES ('PSDB WotLK (245)','PSDB EventAI & ACID 3.0.0',0,NULL);
 /*!40000 ALTER TABLE `db_version` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -49,4 +49,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2009-08-28 22:56:20
+-- Dump completed on 2009-08-31 15:44:15
