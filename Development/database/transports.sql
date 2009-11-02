20b2e57873e701f135f1f067cb5d81ef753b7482
Rog360 <Rog360@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-11-02 18:25:20 +0000

    Added to svn 262... :)
    -- Ruins of Lordaeron Entrance Flags
    -- Giant Pumpkin in Lordaeron
    -- Cockroach Vender spawn fix
    -- Guild NPCs spawn fix
    -- Kraxx WPs
    -- NPC Spawn fixes
    -- Misc GO Spawn fixes
    -- Tawny Grisette WP
    -- Royal Dreadguard Spawn and fix
    -- Andron's Bookshelf spawn fix
    -- Caged NPC emotes
    -- Seeker Nahr random movement
    -- More Mailboxes
    -- Undercity Guardian WP
    -- Susan Tillinghast WP
    -- Forge missing in Rogues Quarter
    -- Kalaen missing spells
    -- Josef Gregorian Now Tailoring Trainer & new spells
    -- Helena Atwood shoots at target
    -- Training Dummies in Undercity
    -- War Quarter spawn fixes
    -- Victor Ward, Basil Frye & Rhiannon Davis are Apprentice
    -- James Van Brunt Now Blacksmithing Trainer
    -- Doctor Martin Felben Apprentice Alchemist
    -- Dan Golthas Apprentice Leatherworker
    -- Arthur Moore Leatherworking Trainer
    -- Lavinia Crowe Enchanting Trainer
    -- Apprentice Enchanter
    -- Supplies not Supplier Etc.
    -- Rhiannon Davis gossip
    -- Victor Ward gossip
    -- Graham Van Talen gossip
    -- Dan Golthas gossip
    -- Doctor Martin Felben gossip
    -- Doctor Marsh gossip
    -- Wren Darkspring in game [Not trainer]
    -- New npc gossip
    -- Furious Gladiator's Armwraps of Triumph
    -- New Gobjects
    -- New Items
    -- New NPC Text
    -- Smith Daelarin is Trainer [Needs Spawn]
    -- Baxter is vendor
    -- Anchorite Yazmina is vendor
    -- Librarian Serrah is vendor
    -- Mrs. Winterby is vendor
    -- Brew Vendor is vendor
    -- Jesse Masters is vendor
    -- New Zeppelin
    -- Tunnel Rat Kobold AI fix
    -- Irradiated Worker spawn fixes & emotes
    -- Frostborn Scouts gossip
    -- mangos_mail_loot_before_update_core Fix

--- Development/database/transports.sql
+++ Development/database/transports.sql
@@ -1,8 +1,8 @@
 -- MySQL dump 10.11
 --
--- Host: localhost    Database: mangos2
+-- Host: localhost    Database: mangos
 -- ------------------------------------------------------
--- Server version	5.0.51b-community-nt
+-- Server version	5.1.32-community
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -23,10 +23,10 @@ DROP TABLE IF EXISTS `transports`;
 SET @saved_cs_client     = @@character_set_client;
 SET character_set_client = utf8;
 CREATE TABLE `transports` (
-  `entry` mediumint(8) unsigned NOT NULL default '0',
+  `entry` mediumint(8) unsigned NOT NULL DEFAULT '0',
   `name` text,
-  `period` mediumint(8) unsigned NOT NULL default '0',
-  PRIMARY KEY  (`entry`)
+  `period` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`entry`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Transports';
 SET character_set_client = @saved_cs_client;
 
@@ -36,7 +36,7 @@ SET character_set_client = @saved_cs_client;
 
 LOCK TABLES `transports` WRITE;
 /*!40000 ALTER TABLE `transports` DISABLE KEYS */;
-INSERT INTO `transports` VALUES (176495,'Grom\'Gol Base Camp and Undercity',315032),(176310,'Menethil Harbor and Auberdine',241778),(176244,'Teldrassil and Auberdine',309295),(176231,'Menethil Harbor and Theramore Isle',230162),(175080,'Grom\'Gol Base Camp and Orgrimmar',248990),(164871,'Orgrimmar and Undercity',239334),(20808,'Ratchet and Booty Bay',231236),(177233,'Forgotton Coast and Feathermoon Stronghold',259751),(181646,'Azuremyst and Auberdine',238707),(190536,'Valiance Keep and Stormwind',271979),(181688,'Valgarde and Menethil',445534),(181689,'Undercity and Vengeance Landing',214579),(186238,'Orgrimmar and Warsong Hold',302705),(186371,'Stolen Zeppelin',484211),(187568,'Moa\'ki Harbor Turtle Boat',445220),(187038,'Pirate boat',307953),(188511,'Unu\'pe Turtle Boat',502354),(192241,'Orgrims Hammer',1424158),(192242,'Fizzcrank Airstrip',1051387);
+INSERT INTO `transports` VALUES (176495,'Grom\'Gol Base Camp and Undercity',315032),(176310,'Menethil Harbor and Auberdine',241778),(176244,'Teldrassil and Auberdine',309295),(176231,'Menethil Harbor and Theramore Isle',230162),(175080,'Grom\'Gol Base Camp and Orgrimmar',248990),(164871,'Orgrimmar and Undercity',239334),(20808,'Ratchet and Booty Bay',231236),(177233,'Forgotton Coast and Feathermoon Stronghold',259751),(181646,'Azuremyst and Auberdine',238707),(190536,'Valiance Keep and Stormwind',271979),(181688,'Valgarde and Menethil',445534),(181689,'Undercity and Vengeance Landing',214579),(186238,'Orgrimmar and Warsong Hold',302705),(186371,'Stolen Zeppelin',484211),(187568,'Moa\'ki Harbor Turtle Boat',445220),(187038,'Pirate boat',307953),(188511,'Unu\'pe Turtle Boat',502354),(192241,'Orgrims Hammer',1424158),(192242,'Fizzcrank Airstrip',1051387),(190549,'Orgrimmar and Thunder Bluff',566000);
 /*!40000 ALTER TABLE `transports` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -49,4 +49,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2008-12-03 18:36:38
+-- Dump completed on 2009-11-02 18:02:53
