6ac8e75ff6318b0f9041242ee3815a27b2614ef9
Rog360 <Rog360@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-11-16 19:32:06 +0000

    Added to SVN 266... :)
    -- NPC Text
    -- Q970 complete script
    -- Young Murk Thresher moved 5 to docks and set neutral
    -- Remove a few of the Coral Sharks at Theramore Docks
    -- Val'anyr, Hammer of Ancient Kings stats fix [Alein]
    -- Dalaran Well areatrigger_teleport [temp fix] [Micks]
    -- Dead Mam'toth Disciple model fix
    -- The Defias Brotherhood end script fix
    -- Hammersmith,Axesmith & Swordsmithing Fix
    -- Captain Brandon,Grondel & Rupert fix
    -- Rodney Wells fix Flight path error
    -- Acherus Deathcharger Speed fix
    -- update all potions  
    -- Reset theses potions to 1  
    -- Reset theses potions to 5
    -- Broken Corpse is dead
    -- New creatures WDB
    -- New gameobjects WDB
    -- New npc_texts WDB
    -- New item_templates WDB
    -- Added by hand for Vendor Above
    -- Items From WDB [Heroic items socket problem]
    -- Maxcount & Stackable fix
    -- statcount fix
    -- disenchant stuff
    -- green armor
    -- green wpn
    -- rares
    -- epix
    -- Divine Hymn Missing from Trainers [Alein]
    -- Bartlett's Bitter Brew no Duration or delay
    -- Scriptname internalitemhanler?
    -- Conqueror's Aegis Breastplate stats fix [Micks]
    -- Conqueror's Aegis Handguards stats fix [Micks]
    -- Conqueror's Aegis Faceguard stats fix [Micks]
    -- Conqueror's Aegis Legguards stats fix [Micks]
    -- Conqueror's Aegis Shoulderguards stats fix [Micks]
    -- Dubin Clay spawn [Micks]
    -- Stormrune Edge, Wrong name [Micks]
    -- mechanic_immune_mask Fixes
    -- Quest Level Changes
    -- Quest Text Fixes
    -- New Creature Templates
    -- New Gameobject Templates
    -- New Npc Text
    -- New Page Text
    -- New Items
    -- mail_level_reward

--- Development/database/mail_level_reward.sql
+++ Development/database/mail_level_reward.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.1.40, for Win32 (ia32)
+-- MySQL dump 10.11
 --
--- Host: localhost    Database: slk
+-- Host: localhost    Database: mangos
 -- ------------------------------------------------------
--- Server version	5.1.40-community
+-- Server version	5.1.32-community
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -20,8 +20,8 @@
 --
 
 DROP TABLE IF EXISTS `mail_level_reward`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `mail_level_reward` (
   `level` tinyint(3) unsigned NOT NULL DEFAULT '0',
   `raceMask` mediumint(8) unsigned NOT NULL DEFAULT '0',
@@ -29,7 +29,7 @@ CREATE TABLE `mail_level_reward` (
   `senderEntry` mediumint(8) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`level`,`raceMask`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Mail System';
-/*!40101 SET character_set_client = @saved_cs_client */;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Dumping data for table `mail_level_reward`
@@ -37,6 +37,7 @@ CREATE TABLE `mail_level_reward` (
 
 LOCK TABLES `mail_level_reward` WRITE;
 /*!40000 ALTER TABLE `mail_level_reward` DISABLE KEYS */;
+INSERT INTO `mail_level_reward` VALUES (20,512,232,16280),(20,1024,227,20914),(20,4,226,4772),(20,64,228,7954),(20,1,224,4732),(20,8,225,4753),(20,2,231,4752),(20,32,229,3690),(20,128,230,7953),(20,16,233,4773),(40,512,272,16280),(40,1024,273,20914),(40,4,274,4772),(40,64,275,7954),(40,1,276,4732),(40,8,277,4753),(40,2,278,4752),(40,32,279,3690),(40,128,280,7953),(40,16,281,4773),(60,690,282,35093),(60,1101,283,35100),(70,1101,284,35133),(70,690,285,35135);
 /*!40000 ALTER TABLE `mail_level_reward` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -49,4 +50,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2009-11-13 20:09:50
+-- Dump completed on 2009-11-16 19:12:24
