cc130de701123167a12f1e2c917b7e1ea6f088dd
braindedd <braindedd@e0e46c49-be69-4f5a-ad62-21024a331aea>
2009-11-27 16:34:05 +0000

    Updated to MaNGOS r8874 / ScriptDev2 r1508

--- Development/database/scriptdev2.sql
+++ Development/database/scriptdev2.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.1.40, for Win32 (ia32)
+-- MySQL dump 10.13  Distrib 5.1.41, for Win32 (ia32)
 --
 -- Host: localhost    Database: slk_sd2
 -- ------------------------------------------------------
--- Server version	5.1.40-community
+-- Server version	5.1.41-community
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -167,7 +167,7 @@ CREATE TABLE `sd2_db_version` (
 
 LOCK TABLES `sd2_db_version` WRITE;
 /*!40000 ALTER TABLE `sd2_db_version` DISABLE KEYS */;
-INSERT INTO `sd2_db_version` VALUES ('ScriptDev2 (for MaNGOS 8831+) ');
+INSERT INTO `sd2_db_version` VALUES ('ScriptDev2 (for MaNGOS 8868+) ');
 /*!40000 ALTER TABLE `sd2_db_version` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -180,4 +180,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2009-11-20 17:35:10
+-- Dump completed on 2009-11-27 18:30:54
