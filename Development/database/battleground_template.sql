2bd31f16dc69fe584a4e0f686ad14da3957ed34a
rog360 <rog360@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-04-19 20:58:02 +0000

    -- Changelog 303 Mostly to fix some errors for move to 3.3.3
    -- i47245 / Pauldrons of Concealed Loathing
    -- Summon Warhorse and Warhorse(Paladin)
    -- Item Disenchant skill with no DisenchantID
    -- New from wdb [one item for 3.3.3]
    -- New GO from cache
    -- Item Startup errors
    -- Some TEMP Achievement_criteria_requirement Fixes
    -- Creature_template_addon spell invalid on aura
    -- Areatrigger_tavern not in DBC
    -- Battleground min/max level fix
    -- Rockjaw Backbreaker in Duskwood
    -- Unstable Voidwalker spawned by Unstable Warp Rift Generator
    -- Portal Kruul & Portal Xilus are untargetable
    -- Quests are for both
    -- Farwhisper 15607 and Grimtotem 15581 spawn fix
    -- Disenchant_loot queries and Statcount fix
    -- quest_poi [Credit UDB]

--- Development/database/battleground_template.sql
+++ Development/database/battleground_template.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.1.45, for Win32 (ia32)
+-- MySQL dump 10.11
 --
--- Host: localhost    Database: slk
+-- Host: localhost    Database: mangos
 -- ------------------------------------------------------
--- Server version	5.1.45-community
+-- Server version	5.1.42-community
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -20,8 +20,8 @@
 --
 
 DROP TABLE IF EXISTS `battleground_template`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `battleground_template` (
   `id` mediumint(8) unsigned NOT NULL,
   `MinPlayersPerTeam` smallint(5) unsigned NOT NULL DEFAULT '0',
@@ -34,7 +34,7 @@ CREATE TABLE `battleground_template` (
   `HordeStartO` float NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Dumping data for table `battleground_template`
@@ -42,7 +42,7 @@ CREATE TABLE `battleground_template` (
 
 LOCK TABLES `battleground_template` WRITE;
 /*!40000 ALTER TABLE `battleground_template` DISABLE KEYS */;
-INSERT INTO `battleground_template` VALUES (1,20,40,51,80,611,2.72532,610,2.27452),(2,5,10,10,80,769,3.14159,770,3.14159),(3,8,15,20,80,890,3.40156,889,0.263892),(7,8,15,61,80,1103,3.40156,1104,0.263892),(4,5,5,10,80,929,0,936,3.14159),(5,5,5,10,80,939,0,940,3.14159),(6,5,5,10,80,0,0,0,0),(8,5,5,10,80,1258,0,1259,3.14159),(9,7,15,71,80,1367,0,1368,0),(10,5,5,10,80,1362,0,1363,0),(11,5,5,10,80,1364,0,1365,0),(30,20,40,71,80,1485,0,1486,0),(32,5,5,0,80,0,0,0,0);
+INSERT INTO `battleground_template` VALUES (1,20,40,51,80,611,2.72532,610,2.27452),(2,5,10,10,80,769,3.14159,770,3.14159),(3,8,15,20,80,890,3.40156,889,0.263892),(7,8,15,61,80,1103,3.40156,1104,0.263892),(4,5,5,10,80,929,0,936,3.14159),(5,5,5,10,80,939,0,940,3.14159),(6,5,5,10,80,0,0,0,0),(8,5,5,10,80,1258,0,1259,3.14159),(9,7,15,71,80,1367,0,1368,0),(10,5,5,10,80,1362,0,1363,0),(11,5,5,10,80,1364,0,1365,0),(30,20,40,71,80,1485,0,1486,0),(32,5,5,10,80,0,0,0,0);
 /*!40000 ALTER TABLE `battleground_template` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -55,4 +55,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2010-04-16 13:54:34
+-- Dump completed on 2010-04-19 20:49:05
--- Development/database/db_version.sql
+++ Development/database/db_version.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.1.45, for Win32 (ia32)
+-- MySQL dump 10.11
 --
--- Host: localhost    Database: slk
+-- Host: localhost    Database: mangos
 -- ------------------------------------------------------
--- Server version	5.1.45-community
+-- Server version	5.1.42-community
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -20,15 +20,15 @@
 --
 
 DROP TABLE IF EXISTS `db_version`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `db_version` (
   `version` varchar(120) DEFAULT NULL,
   `creature_ai_version` varchar(120) DEFAULT NULL,
   `cache_id` int(10) DEFAULT '0',
   `required_9752_01_mangos_gameobject_template` bit(1) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Used DB version notes';
-/*!40101 SET character_set_client = @saved_cs_client */;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Dumping data for table `db_version`
@@ -36,7 +36,7 @@ CREATE TABLE `db_version` (
 
 LOCK TABLES `db_version` WRITE;
 /*!40000 ALTER TABLE `db_version` DISABLE KEYS */;
-INSERT INTO `db_version` VALUES ('PSDB WotLK (r303) for 3.3.3a (Core r9752 / SD2 r1671)','PSDB EventAI & ACID 3.0.3',303,NULL);
+INSERT INTO `db_version` VALUES ('PSDB WotLK (r304) for 3.3.3a (Core r9752 / SD2 r1671)','PSDB EventAI & ACID 3.0.3',304,NULL);
 /*!40000 ALTER TABLE `db_version` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -49,4 +49,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2010-04-16 13:53:08
+-- Dump completed on 2010-04-19 20:49:48
