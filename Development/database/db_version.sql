f57dfdb853ddbfd15207654436f31b4143f08a41
Rog360 <Rog360@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-10-18 18:02:18 +0000

    -- Changelog 339 --
    -- Past Endeavors QT changed
    -- Shardtooth Meat Quest Drop Only
    -- Moonglade Wardens WP
    -- Creature falls thru map
    -- Reagent Pouch Drop Fix
    -- EventAI Fix
    -- Generic_creature script Removed [Has EventAI]
    -- Arcane Disturbances quest fix
    -- Misc. spawn fixes
    -- Scarlet Gryphon Rider fix
    -- Scarlet Gryphon fix
    -- Plague Spreader WP & fix
    -- Abandoned Mail item & Quest fix
    -- Healthy Dragon Scale Loot Fix
    -- The Deed to Caer Darrow & Brill Not looted from creatures
    -- Zap Farflinger Text Fix
    -- Blood of Innocents Loot Fix
    -- Minion of Gurok summoned by i24501
    -- Bone Minion summoned by c16118
    -- Bone Mage summoned by c16118
    -- Ambient Minion of Terokk Flys
    -- Illusion of Jandice Barov spawned by c10503
    -- Brazier of the Herald spawn
    -- Kormok temp spawn [Needs SD2]
    -- New from WDB [Credit some data to YTDB & UDB]
    -- New Modelids [UDB]
    -- Wood Frog Fix
    -- Tree Frog Fix
    -- Captain's Footlocker
    -- Kill credit fix
    -- New Model Fixes
    -- Acid 3.0.6
    -- WDB Creature Fixes
    -- i42989 / Darkmoon Card: Berserker [Micks]
    -- Stats Fix [Micks]
    -- Death Knight [Micks]
    -- i51305 / Sanctified Scourgelord Chestguard
    -- i51222 / Sanctified Ymirjar Lord's Handguards
    -- i51134 / Sanctified Scourgelord Chestguard
    -- i51132 / Sanctified Scourgelord Handguards
    -- i50857 / Scourgelord Chestguard
    -- i50856 / Scourgelord Handguards
    -- Paladin [Micks]
    -- i51265 / Sanctified Lightsworn Chestguard
    -- i51267 / Sanctified Lightsworn Handguards
    -- i51174 / Sanctified Lightsworn Chestguard
    -- i51172 / Sanctified Lightsworn Handguards
    -- i50864 / Lightsworn Chestguard
    -- i50863 / Lightsworn Handguards
    -- Recipe: Kungaloosh [Micks]
    -- Recipe: Succulent Orca Stew [Micks]
    -- Portal Ebon Hold to Stormwind phasemask [Micks]
    -- Portal Ebon Hold to Orgrimmar phasemask [Micks]
    -- Silver Covenant Agent Attackable [Micks]
    -- Toravon the Ice Watcher Instancebind [Micks]
    -- Azure Captain attackable (Violet Hold) [Micks]
    -- Frozen Orb SellPrice [Micks]
    -- Sons of Hodir / Storm Peaks [Micks]
    -- Correct spawn Flight master [Micks]
    -- Njormeld [Micks]
    -- Lillehoff Spawmask [Micks]
    -- King Jokkum [Micks]
    -- Hodir's Horn [Micks]
    -- o192524 / Arngrim the Insatiable Object [Micks]
    -- Arngrim chair objects [ytdb]
    -- Farshire Grain Credit [Compounds]
    -- Tracker Thulin [Compounds]
    -- Tournament Hippogryph [Compounds]
    -- Akiak [Compounds]
    -- Trag Highmountain [Compounds]
    -- Argent Valiant [Compounds]
    -- Darnassus Valiant [Compounds]
    -- Argent Peacekeeper [Compounds]
    -- Rugan Steelbelly [Compounds]
    -- Sergent Amelyn [Compounds]
    -- Guard Lasiter [Compounds]
    -- Smiling Jim [Compounds]
    -- Theramore Archery Target 1 [Compounds]
    -- Theramore Archery Target 2 [Compounds]
    -- Calia Hastings [Compounds]
    -- Deserter Agitators [Compounds]
    -- Falgran Hastil [Compounds]
    -- Privateer Groy [Compounds]
    -- The Collector Chain quest fix [Bisonte]
    -- Q10060 & 10054 race fix [Bisonte]

--- Development/database/db_version.sql
+++ Development/database/db_version.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.1.51, for Win64 (unknown)
+-- MySQL dump 10.11
 --
--- Host: localhost    Database: slk
+-- Host: localhost    Database: mangos
 -- ------------------------------------------------------
--- Server version	5.1.51-community
+-- Server version	5.1.42-community
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -20,15 +20,15 @@
 --
 
 DROP TABLE IF EXISTS `db_version`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `db_version` (
   `version` varchar(120) DEFAULT NULL,
   `creature_ai_version` varchar(120) DEFAULT NULL,
   `cache_id` int(10) DEFAULT '0',
   `required_10604_01_mangos_spell_proc_event` bit(1) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Used DB version notes';
-/*!40101 SET character_set_client = @saved_cs_client */;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Dumping data for table `db_version`
@@ -36,7 +36,7 @@ CREATE TABLE `db_version` (
 
 LOCK TABLES `db_version` WRITE;
 /*!40000 ALTER TABLE `db_version` DISABLE KEYS */;
-INSERT INTO `db_version` VALUES ('PSDB WotLK (r338) for 3.3.5a (Core r10604 / SD2 r1842)','PSDB EventAI & ACID 3.0.3',338,NULL);
+INSERT INTO `db_version` VALUES ('PSDB WotLK (r339) for 3.3.5a (Core r10604 / SD2 r1842)','ACID 3.0.6 - Full Release for Mangos (3.3.5a Client)',339,NULL);
 /*!40000 ALTER TABLE `db_version` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -49,4 +49,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2010-10-16 11:57:50
+-- Dump completed on 2010-10-18 17:51:52
