5b7ea9fc27b16e16aafd0dc4fdaeb0814470ef2b
Rog360 <Rog360@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-11-22 18:12:36 +0000

    -- Changelog 345 --
    -- Kriss Goldenlight & Aurora Skycaller Spawn fix
    -- Gold Coins Fixes
    -- Missing Objects for GE
    -- The Demoniac Scryer Fix
    -- Graveyard Fix
    -- Warp-Huntress Kula AI
    -- Back Biter Remove Spawn [Summoned]
    -- Garl Grimgrizzle AI
    -- Tabitha Remove Spawn [Summoned]
    -- Tabitha Pet Lvl Stats
    -- Hive'Zara Hatchling Summoned by Buru Egg
    -- Wanathan WP
    -- Shokavis WP
    -- Mimbihi WP
    -- Umbiwa WP
    -- Abohba WP
    -- Magister Hathorel WP
    -- Blackflight Arrow Buycount Fix
    -- Missing Doodad_InstancePortals CFR
    -- Misc. Spawn Fix
    -- SpawnTime Fix
    -- Balance Must Be Preserved Fix
    -- Feralfen Idol Loot Fix
    -- Tamed Sporebat Summoned by c18113 and No Loot
    -- Mature Spore Sac Not Drop From creatures
    -- Bog Lord Tendril Drops without Quest
    -- Sporebat Eye Quest loot fix
    -- Scout Jyoba's Report Loot Fix
    -- Some item converts
    -- Gossip Fix [UDB]
    -- More gossip Fixes
    -- Higher learning achievement Fix [UDB]
    -- Achievement "Dead Man's Party" [UDB]
    -- Add Menu for Hello
    -- Even more gossip menu Fixes
    -- npc text [WDB]
    -- Quest POI [UDB]
    -- GO New [WDB]
    -- Aku'mai Servant Stats Fix
    -- Misc. Faction,Gold & unit_class Fix
    -- Light-Blessed Relic required target
    -- Life Spark stats Fix
    -- Plague Spray required target
    -- Sanctum Sentry Stats Fix
    -- Feral Defender Stats Fix
    -- Item Fixes [WDB]
    -- Treat Bag Loot [UDB]
    -- Rusted Prisoner's Footlocker Loot [UDB]
    -- creature_model_info Fixes [UDB]
    -- New NPCs 3.3.5
    -- Melnan Darkstone HP Fix
    -- Npc_gossip wrong or Already in gossip_menu
    -- Draenei Anchorite Faction Fix
    -- Draenei Vindicator Faction Fix
    -- Fish only water movement
    -- Sawfin Frenzy No Loot
    -- Blood Knight Dawnstar Faction Fix
    -- Reef Frenzy Spawns
    -- Whitetail Frenzy Spawns
    -- More achievement Fixes
    -- Shizz Work Fix
    -- Hemie143 Fixes
    -- Lady Jaina Proudmoore c37597 (FoS entrance) gossip fix
    -- Dead Caravan Guard loot fix
    -- Dead Caravan Worker loot fix
    -- Dead Caravan Guard and Worker location overlap fix
    -- Dead Caravan Worker model fix
    -- Q10368 Fix
    -- Morod the Windstirrer gossip fix - c20677
    -- Akoru the Firecaller gossip fix - c20678
    -- Aylaan the Waterwaker gossip fix - c20679
    -- Scoodles spawn fix for Scoodles quest q11510
    -- Q12075 Workaround
    -- Bisonte Fixes
    -- Learning The Reins Fix
    -- Micks Fixes
    -- Restore Alterac npcs and game objects
    -- Mini Thor
    -- Charscale Assaulter / spawn time
    -- Charscale Invoker / spawn time
    -- Charscale Commander / spawn time
    -- Charscale Elite / spawn time
    -- Halion / Baltharus the Warborn / General Zarithrian / Saviana Ragefire - spawn time
    -- Halion / flags
    -- twilight portal REal
    -- General Zarithrian 10
    -- General Zarithrian 25
    -- Saviana Ragefire 10
    -- 25
    -- Baltharus the Warborn 10
    -- 25
    -- Toravon the Ice Watcher
    -- Frostmourne

--- Development/database/db_version.sql
+++ Development/database/db_version.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.1.52, for Win64 (unknown)
+-- MySQL dump 10.11
 --
--- Host: localhost    Database: slk
+-- Host: localhost    Database: mangos
 -- ------------------------------------------------------
--- Server version	5.1.52-community
+-- Server version	5.1.42-community
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -20,15 +20,15 @@
 --
 
 DROP TABLE IF EXISTS `db_version`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `db_version` (
   `version` varchar(120) DEFAULT NULL,
   `creature_ai_version` varchar(120) DEFAULT NULL,
   `cache_id` int(10) DEFAULT '0',
   `required_10743_02_mangos_spell_bonus_data` bit(1) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Used DB version notes';
-/*!40101 SET character_set_client = @saved_cs_client */;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Dumping data for table `db_version`
@@ -36,7 +36,7 @@ CREATE TABLE `db_version` (
 
 LOCK TABLES `db_version` WRITE;
 /*!40000 ALTER TABLE `db_version` DISABLE KEYS */;
-INSERT INTO `db_version` VALUES ('PSDB WotLK (r344) for 3.3.5a (Core r10743 / SD2 r1882)','ACID 3.0.7 - Full Release for Mangos (3.3.5a Client)',344,NULL);
+INSERT INTO `db_version` VALUES ('PSDB WotLK (r345) for 3.3.5a (Core r10743 / SD2 r1882)','ACID 3.0.7 - Full Release for Mangos (3.3.5a Client)',345,NULL);
 /*!40000 ALTER TABLE `db_version` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -49,4 +49,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2010-11-19 17:21:46
+-- Dump completed on 2010-11-22 17:55:38
