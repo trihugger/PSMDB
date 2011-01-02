ce8dbb5e72c4fd43250f1a37f3cecf08549ab63d
braindedd <braindedd@e0e46c49-be69-4f5a-ad62-21024a331aea>
2011-01-02 16:03:10 +0000

    Updated to MaNGOS r10949 / ScriptDev2 r1918

--- Development/database/game_event_creature_data.sql
+++ Development/database/game_event_creature_data.sql
@@ -16,28 +16,32 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `db_version`
+-- Table structure for table `game_event_creature_data`
 --
 
-DROP TABLE IF EXISTS `db_version`;
+DROP TABLE IF EXISTS `game_event_creature_data`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `db_version` (
-  `version` varchar(120) DEFAULT NULL,
-  `creature_ai_version` varchar(120) DEFAULT NULL,
-  `cache_id` int(10) DEFAULT '0',
-  `required_10949_01_mangos_mangos_string` bit(1) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Used DB version notes';
+CREATE TABLE `game_event_creature_data` (
+  `guid` int(10) unsigned NOT NULL DEFAULT '0',
+  `entry_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `modelid` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `equipment_id` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `spell_start` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `spell_end` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `event` smallint(5) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`guid`,`event`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `db_version`
+-- Dumping data for table `game_event_creature_data`
 --
 
-LOCK TABLES `db_version` WRITE;
-/*!40000 ALTER TABLE `db_version` DISABLE KEYS */;
-INSERT INTO `db_version` VALUES ('PSDB WotLK (r349) for 3.3.5a (Core r10949 / SD2 r1918)','ACID 3.0.7 - Full Release for Mangos (3.3.5a Client)',349,NULL);
-/*!40000 ALTER TABLE `db_version` ENABLE KEYS */;
+LOCK TABLES `game_event_creature_data` WRITE;
+/*!40000 ALTER TABLE `game_event_creature_data` DISABLE KEYS */;
+INSERT INTO `game_event_creature_data` VALUES (13018,0,0,504,0,0,27),(13019,0,0,504,0,0,27),(13020,0,0,504,0,0,27),(13021,0,0,504,0,0,27),(13022,0,0,504,0,0,27),(13023,0,0,504,0,0,27),(13024,0,0,504,0,0,27),(13025,0,0,504,0,0,27),(13026,0,0,504,0,0,27),(13027,0,0,504,0,0,27);
+/*!40000 ALTER TABLE `game_event_creature_data` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -49,4 +53,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2011-01-02 17:59:05
+-- Dump completed on 2011-01-02 17:59:55
