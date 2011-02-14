74ce92e7c7c1aa3227e35167f2eb1248832c99b0
Rog360 <Rog360@e0e46c49-be69-4f5a-ad62-21024a331aea>
2011-02-14 04:34:02 +0000

    ------------------- -- CHANGELOG 354 -- -------------------
    -- Suntouched Special Reserve Loot Fix
    -- Bloodthistle Plant Loot Fix
    -- Ivory Bell Loot Fix
    -- Q10488: Protecting Our Own Fix
    -- [NPC]21840: Cosmetic Silkwings Fix
    -- [NPC]19978: Deathforge Over-Smith Faction Fix
    -- Infused Vekh'nir Crystal Quest Drop Fix
    -- Destructo-Blade Not Quest Drop
    -- creature_template flags_extra Fix
    -- Fishing Junk Fix
    -- Unsealed Chest Loot Fix
    -- Speaker Mar'grom Loot Group Fix
    -- Goretooth Loot Group Fix
    -- Misc Spawn Fixes
    -- Q10609 NPCs Summoned by 20021 Fix
    -- Savannah Cub Summoned by c3416
    -- Lesser Nether Drake Fix [Too many]
    -- Grulloc's Dragon Skull Spawn on death Fix
    -- Daggerfen Poison Manual Dupe Fix
    -- Crystal Flayer Spawn Fix
    -- Wyrm from Beyond Passive Fix
    -- Shattered Sun Marksman Model Fix
    -- Shattered Sun Warrior Model Fix
    -- Mutated Farahlon Lasher Summoned By I29818
    -- Flora of the Eco-Domes Quest Fix
    -- Lord Sanguinar Rank fix
    -- Grand Astromancer Capernian fix
    -- Master Engineer Telonicus fix
    -- Thaladred the Darkener fix
    -- Farahlon Lasher Add EventAI
    -- Mark V is Alive! Quest Fix
    -- Recharging the Batteries Quest Fix
    -- Phase Hunter EventAI Fix
    -- Drained Phase Hunter Spawned By I28369
    -- Lagoon Steam Pump Credit Marker Spawn Fix
    -- Gorkan Bloodfist Gossip Flag Fix
    -- Kernda Gossip Flag Fix
    -- Love is in the Air Event Quests Fix
    -- Dupe Bench in Silvermoon
    -- Sign fixes
    -- Lazy Peon EventAI Fix
    -- Skullsplitter Hunter Spawns Needed
    -- Skullsplitter Berserker Spawns Needed
    -- Shrine of the Hawk Fix
    -- Darkmoon Fortune Teller Spawned by Decks
    -- Southsea Brigand Model Fix
    -- Baron Longshore Random Spawns Fix
    -- Isha Awak WP
    -- The Horde Needs Your Help! Not in Game
    -- Silithid Swarm Not IN The Barrens
    -- Silithid Grub Summoned by Silithid Creeper Egg[c5781]
    -- Silithid Creeper Egg Summoned by Silithid Creeper[c3250]
    -- Big Electromental Flavor Not move & Fly Fix
    -- Scalewing Serpent Bad Spawn & Addon
    -- Dupe Spirit Healer
    -- Razak Ironsides Dupe & WPs Fix
    -- Ride the Lightning q10657 Workaround Fix
    -- Blade's Edge Orbs Fly Fix
    -- Game Event Error Fix
    -- Lord Alexei Barov EventAI [SD2 Removed]
    -- Instructor Malicia EventAI [SD2 Removed
    -- Ras Frostwhisper EventAI [SD2 Removed]
    -- The Ravenian EventAI [SD2 Removed]
    -- Lady Illucia Barov EventAI [SD2 Removed]
    -- Doctor Theolen Krastinov EventAI [SD2 Removed]
    -- Lorekeeper Polkelt EventAI [SD2 Removed]
    -- Blastmaster Emi Shortfuse Spawn Fix [SD2]
    -- The Glowing Shard Not Autocomplete
    -- Venture Co. Logger Add 3 Spawns
    -- Prisoner of Jin'Zil In Cages
    -- Deepmoss Matriarch Summoned by Deepmoss Hatchling
    -- Creatures Not have Spirit Bond Aura [Hunter Ability Aura]
    -- Skoll Pool 1 Fix
    -- Azure Warder 1 Pool Fix
    -- Azure Magus 1 Pool Fix
    -- c4425 117 Pool Fix
    -- c4438 117 Pool Fix
    -- c5912 117 Pool Fix
    -- c6488 117 Pool Fix
    -- c3586 117 Pool Fix
    -- c1720 117 Pool Fix
    -- c6489 118 Pool Fix
    -- c9024 c9041 c9042 118 Pool Fix
    -- c10809 c10558 118 Pool Fix
    -- c38453 118 Pool Fix
    -- c10376 c9596 c10263 c10899 116 Pool Fix
    -- c6490 30 Pool Fix
    -- Pool Fix For 40,59,60,89
    -- Deathsworn Captain Added Back to pool_creature
    -- Zerillis Added Back to pool_creature
    -- Azurous Added Back to pool_creature
    -- Jed Runewatcher Added Back to pool_creature
    -- Zekkis Added Back to pool_creature
    -- Veyzhak the Cannibal Added Back to pool_creature
    -- XT:9 WPs
    -- XT:4 WPs
    -- Wrathscale Fountain Spawn Fix
    -- Hezrul Bloodmark WPs
    -- Enraged Panther Not Move [Caged]
    -- Panther Cage Size Fix

--- Development/database/db_version.sql
+++ Development/database/db_version.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.5.9, for Win64 (x86)
+-- MySQL dump 10.11
 --
--- Host: localhost    Database: slk
+-- Host: localhost    Database: mangos
 -- ------------------------------------------------------
--- Server version	5.5.9
+-- Server version	5.5.8
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -20,15 +20,15 @@
 --
 
 DROP TABLE IF EXISTS `db_version`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `db_version` (
   `version` varchar(120) DEFAULT NULL,
   `creature_ai_version` varchar(120) DEFAULT NULL,
   `cache_id` int(10) DEFAULT '0',
   `required_11117_01_mangos_world_template` bit(1) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Used DB version notes';
-/*!40101 SET character_set_client = @saved_cs_client */;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Dumping data for table `db_version`
@@ -36,7 +36,7 @@ CREATE TABLE `db_version` (
 
 LOCK TABLES `db_version` WRITE;
 /*!40000 ALTER TABLE `db_version` DISABLE KEYS */;
-INSERT INTO `db_version` VALUES ('PSDB WotLK (r353) for 3.3.5a (Core r11117 / SD2 r1973)','ACID 3.0.7 - Full Release for Mangos (3.3.5a Client)',353,NULL);
+INSERT INTO `db_version` VALUES ('PSDB WotLK (r354) for 3.3.5a (Core r11117 / SD2 r1973)','ACID 3.0.7 - Full Release for Mangos (3.3.5a Client)',354,NULL);
 /*!40000 ALTER TABLE `db_version` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -49,4 +49,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2011-02-11 17:13:20
+-- Dump completed on 2011-02-14  4:22:21
