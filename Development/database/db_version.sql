3d2755c962ee72e60a9e2933ae00f534db13dade
braindedd <braindedd@e0e46c49-be69-4f5a-ad62-21024a331aea>
2011-05-21 10:32:50 +0000

    Updated to MaNGOS r11503 / ScriptDev2 r2116

--- Development/database/db_version.sql
+++ Development/database/db_version.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.5.9, for Win64 (x86)
+-- MySQL dump 10.13  Distrib 5.5.12, for Win64 (x86)
 --
 -- Host: localhost    Database: slk
 -- ------------------------------------------------------
--- Server version	5.5.9
+-- Server version	5.5.12
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -26,7 +26,7 @@ CREATE TABLE `db_version` (
   `version` varchar(120) DEFAULT NULL,
   `creature_ai_version` varchar(120) DEFAULT NULL,
   `cache_id` int(10) DEFAULT '0',
-  `required_11234_01_mangos_command` bit(1) DEFAULT NULL
+  `required_11503_01_mangos_spell_proc_event` bit(1) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Used DB version notes';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -36,7 +36,7 @@ CREATE TABLE `db_version` (
 
 LOCK TABLES `db_version` WRITE;
 /*!40000 ALTER TABLE `db_version` DISABLE KEYS */;
-INSERT INTO `db_version` VALUES ('PSDB WotLK (r362) for 3.3.5a (Core r11234 / SD2 r1990)','ACID 3.0.7 - Full Release for Mangos (3.3.5a Client)',362,NULL);
+INSERT INTO `db_version` VALUES ('PSDB WotLK (r363) for 3.3.5a (Core r11503 / SD2 r2116)','ACID 3.0.7 - Full Release for Mangos (3.3.5a Client)',363,NULL);
 /*!40000 ALTER TABLE `db_version` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -49,4 +49,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2011-03-11 23:36:15
+-- Dump completed on 2011-05-21 12:23:14
