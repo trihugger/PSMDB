3870d332d9a1e566fd42cf65cd9eec6790e813e7
Rog360 <Rog360@e0e46c49-be69-4f5a-ad62-21024a331aea>
2013-01-07 21:44:23 +0000

    Removed replaced table by core... :)

--- Development/database/db_version.sql
+++ Development/database/db_version.sql
@@ -1,6 +1,6 @@
--- MySQL dump 10.13  Distrib 5.5.27, for Win32 (x86)
+-- MySQL dump 10.11
 --
--- Host:     Database: mangos
+-- Host: localhost    Database: mangos
 -- ------------------------------------------------------
 -- Server version	5.5.27
 
@@ -20,15 +20,15 @@
 --
 
 DROP TABLE IF EXISTS `db_version`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
 CREATE TABLE `db_version` (
   `version` varchar(120) DEFAULT NULL,
   `creature_ai_version` varchar(120) DEFAULT NULL,
   `cache_id` int(10) DEFAULT '0',
   `required_12279_01_mangos_creature_template` bit(1) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Used DB version notes';
-/*!40101 SET character_set_client = @saved_cs_client */;
+SET character_set_client = @saved_cs_client;
 
 --
 -- Dumping data for table `db_version`
@@ -36,7 +36,7 @@ CREATE TABLE `db_version` (
 
 LOCK TABLES `db_version` WRITE;
 /*!40000 ALTER TABLE `db_version` DISABLE KEYS */;
-INSERT INTO `db_version` VALUES ('PSDB (r379) for 3.3.5a (Core r12279 / SD2 r2780)','ACID 3.1.0 \'Another Wild Adventure\' - Full Release for MaNGOS (3.3.5a Client)',379,NULL);
+INSERT INTO `db_version` VALUES ('PSDB (r380) for 3.3.5a (Core r12279 / SD2 r2780)','ACID 3.1.0 \'Another Wild Adventure\' - Full Release for MaNGOS (3.3.5a Client)',380,NULL);
 /*!40000 ALTER TABLE `db_version` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -49,4 +49,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2013-01-07 16:01:05
+-- Dump completed on 2013-01-07 21:42:30
