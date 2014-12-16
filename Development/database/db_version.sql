5372abd169f091e486945fff5eb6989e1a880c1f
Rog360 <Rog360@e0e46c49-be69-4f5a-ad62-21024a331aea>
2014-12-16 01:31:22 +0000

    ------------------- -- CHANGELOG 392 -- -------------------
    -- Stable Master Spawn fix
    -- Spell Script Target Fixes
    -- Q3908 Complete Script Fix
    -- Tarwyns Fixes
    -- Deadmines Cleanup
    -- Remove Test Dummy
    -- Razorfen Downs
    -- Fix Withered Spearhide max HP
    -- Remove Lady Faltheress (NPC only available during Scourge Invasion)
    -- Razorfen Kraul
    -- Improvements for quest 1221 Blueleaf Tubers
    -- Add GO 300011 TEMP Nearby Tubers at all spawnpoints of Blueleaf Tubers (GO 20920)
    -- fix spell target diameter
    -- C4781 Stats Fix
    -- End of Tarwyns Fixes 
    -- Captain Greenskin WPs & Follower Fix
    -- Q24849 and 24851 Fix
    -- Call the Headless Horseman Quests Fixes
    -- Q5203 Tweeked for SD2 Fix
    -- Q4261 Tweeked for SD2 Fix
    -- Q4442 Start Script Fix
    -- Q10347 PrevQuest Fix
    -- Blastmaster Emi Shortfuse Event Fix
    -- Bubbly Fissure Spawn fix
    -- Fel Reaver WP Fix
    -- Q10757 Complete Script Fix
    -- Ore Pool Adjusted
    -- Herb Pool Adjusted
    -- Q12027 now done with SD2 Fix
    -- Q778 Start Script Fix
    -- Misc Faction Fixes
    -- Q12701 Fix
    -- SaDracos Fixes
    -- Dalaran Fixes
    -- Despawn Creature at a point
    -- Dalaran Visitor
    -- Jean Pierre Poulain
    -- End of SaDracos Fixes
    -- Q6041 Fix [Needs SD2]
    -- Q10106 & 10110 Flag Fix
    -- HellFire GO Stats Fix
    -- HellFire Capture Point Fix
    -- Q10547 Loot Fix
    -- Q10514 Fix
    -- C22265 Spawn dist & z pos Fix
    -- C17969 Speed Fix
    -- C20415 Speed Fix
    -- NPC Stat Fixes
    -- Curmana Fixes
    -- Curhealth Fixes
    -- Q5050 Previous Quest Fix
    -- Spirit of Minshina Not Move/Attack Fix
    -- Missing Fishing Loot Fix
    -- NPC Linking Fix
    -- Items Fix
    -- The Ancient Lift Fix
    -- Many Fixes done by SD2
    -- Many Fixes done by CMangos
    
    Thanks to all involved. Have a Merry Christmas and a Happy & Safe New Year!

--- Development/database/db_version.sql
+++ Development/database/db_version.sql
@@ -26,7 +26,7 @@ CREATE TABLE `db_version` (
   `version` varchar(120) DEFAULT NULL,
   `creature_ai_version` varchar(120) DEFAULT NULL,
   `cache_id` int(10) DEFAULT '0',
-  `required_12748_01_mangos_spell_template` bit(1) DEFAULT NULL
+  `required_12759_01_mangos_spell_chain` bit(1) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Used DB version notes';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -36,7 +36,7 @@ CREATE TABLE `db_version` (
 
 LOCK TABLES `db_version` WRITE;
 /*!40000 ALTER TABLE `db_version` DISABLE KEYS */;
-INSERT INTO `db_version` VALUES ('PSDB (r391) for 3.3.5a (Core r12748/SD2 r3050)','ACID 3.3.5a (Wrath of the Lich King) for CMaNGOS-WOTLK (3.3.5a Client)',391,NULL);
+INSERT INTO `db_version` VALUES ('PSDB (r392) for 3.3.5a (Core r12759/SD2 r3071)','ACID 3.3.5a (Wrath of the Lich King) for CMaNGOS-WOTLK (3.3.5a Client)',392,NULL);
 /*!40000 ALTER TABLE `db_version` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -49,4 +49,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-09-22 17:41:31
+-- Dump completed on 2014-12-15 20:03:38
